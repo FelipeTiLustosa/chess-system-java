992baf1b4f5b5827085b1b96a2574a2e0130de40
Felipe Lustosa <lustosaf590@gmail.com>
2024-09-12 22:00:45 -0300

    Pawn

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -6,7 +6,7 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece {
+public class Rook extends ChessPiece {// Torre
 
     public Rook(Board board, Color color) {
         super(board, color);
